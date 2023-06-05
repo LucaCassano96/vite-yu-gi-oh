265453f550ed04a0291e1fd93a7da662c5d4f9f1
Luca Cassano <lucacassano96@gmail.com>
2023-06-05 16:32:51 +0200

    title

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+    name: "AppHeader"
+}
+
+</script>
+
+<template>
+    <header>
+
+        <div id="container">
+
+            <h1>YU-GI-OH API</h1>
+
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+/* @use ".styles/partials/variables.scss" as*; */
+@use "src/styles/general.scss" as *;
+@use "src/styles/partials/variables.scss" as*;
+
+header {
+    width: 100%;
+    height: 80px;
+
+    #container {
+        @include container;
+    }
+}
+</style>
